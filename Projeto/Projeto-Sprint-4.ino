f0542d0e5bc6cf0451c558c7b1ce3ed58a69b023
Vitorhrds <110406986+Vitorhrds2@users.noreply.github.com>
2023-05-11 15:29:34 -0300

    Código do projeto pronto - Sprint 2 (semana 4)
    A funcionalidade de conectar e desconectar em um wifi fictício foi adicionada e mostrando os resultados em um LCD com o módulo I2C e um led através de um botão.

---
+++ Projeto/Projeto-Sprint-4.ino
@@ -0,0 +1,95 @@
+#include <LiquidCrystal_I2C.h>
+
+// Declaração das constantes e pinos
+int ledVermelho = 16;
+int nivelBotao;
+int botao = 15;
+int estadoBotao = 0;
+
+// Declaração do objeto do LCD
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+// Variáveis de controle de tempo e contador
+int tempoContador = 3;
+int contadorExibicao = 0;
+String mensagem;
+String ip;
+
+// Função de inicialização
+void configurar()
+{
+  lcd.begin();
+  lcd.noBacklight();
+  Serial.begin(115200);
+
+  pinMode(botao, OUTPUT);      // Define o botão como saída
+  pinMode(ledVermelho, OUTPUT); // Define o LED como saída
+}
+
+// Função que exibe a mensagem de conexão no LCD
+void exibirMensagemConexao(String ip)
+{
+  tempoContador = 0;
+  lcd.print("Conectando");
+  delay(1000);
+
+  while (tempoContador <= 9)
+  {
+    if (contadorExibicao == 3)
+    {
+      lcd.clear();
+      lcd.print("Conectando");
+      contadorExibicao = 0;
+    }
+    lcd.print(".");
+    delay(1000);
+    tempoContador++;
+    contadorExibicao++;
+  }
+
+  lcd.clear();
+  lcd.print("Conectado a");
+  lcd.setCursor(0, 1);
+  lcd.print("rede Wifi");
+  delay(3000);
+  lcd.clear();
+  lcd.print("IP do ESP local: ");
+  lcd.setCursor(0, 1);
+  lcd.print(ip);
+}
+
+// Função para lidar com os eventos do botão
+void lidarComEventosBotao()
+{
+  nivelBotao = digitalRead(botao);
+
+  if (nivelBotao == 1 && estadoBotao == 0)
+  {
+    estadoBotao = 1;
+
+    lcd.backlight();
+    lcd.display();
+    lcd.clear();
+
+    exibirMensagemConexao("12.345.678.9");
+
+    digitalWrite(ledVermelho, HIGH);
+    delay(1000);
+    Serial.println(String(estadoBotao));
+  }
+  else if (nivelBotao == 1 && estadoBotao == 1)
+  {
+    estadoBotao = 0;
+    lcd.clear();
+    lcd.print("Desconectado");
+    digitalWrite(ledVermelho, LOW);
+    delay(2000);
+    lcd.noBacklight();
+    lcd.noDisplay();
+  }
+}
+
+void loop()
+{
+  lidarComEventosBotao();
+}
